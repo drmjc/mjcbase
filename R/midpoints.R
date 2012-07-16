917bec525192f192b93d057cd33e2816b60206c7
Mark Cowley <m.cowley0@gmail.com>
2012-07-16 05:01:26 +0000

    added sorted check roxygenized

--- R/midpoints.R
+++ R/midpoints.R
@@ -1,9 +1,20 @@
-## Function that works out the midpoints between a vector of numbers,
-## eg midpoints(c(1,5,9)) will return c(3, 5)
-##
-## Mark Cowley, 30 June 2005
-##
+#' Determine the midpoints between a vector of numbers
+#' 
+#' @param x a numeric vector of length >= 2. if unsorted, then it
+#'	will be sorted. \code{NA}'s will be excluded.
+#' @return a numeric vector of midpoints, of length n-1
+#' 
+#' @author Mark Cowley, 30 June 2005
+#' @export
+#' @examples
+#' midpoints(c(1,5,9))
+#' # [1] 3 7
+#'
 midpoints <- function(x) {
+	x <- x[!is.na(x)]
+	if( is.unsorted(x) ) {
+		x <- sort(x, decreasing=FALSE)
+	}
 	res <- rep(0, length(x)-1)
 	for(i in 1:(length(x)-1)) {
 		res[i] <- round(mean(c(x[i], x[i+1])), 0)
