e605f24cef192754619c7bb655a3a6fd76e30f1e
Mark Cowley <m.cowley0@gmail.com>
2012-07-16 05:03:09 +0000

    made Defunct roxygenized

--- R/rm.na.R
+++ R/rm.na.R
@@ -1,2 +1,12 @@
-rm.na <- function(x) return(x[!is.na(x)])
-
+#' remove NA's from an object
+#'
+#' @note Defunct: \code{\link{na.omit}} is a better alternative.
+#' 
+#' @param x a 1D object
+#' @return an object with no \code{NA}'s
+#' @author Mark Cowley, 2012-07-16
+#' @export
+rm.na <- function(x) {
+	.Defunct(na.omit)
+	return(x[!is.na(x)])
+}
