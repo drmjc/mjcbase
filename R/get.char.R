2613d1e905dd0e4a9a0d5fe9ebe88052cd86f083
Mark Cowley <m.cowley0@gmail.com>
2012-07-16 04:51:08 +0000

    updated code roxygenized

--- R/get.char.R
+++ R/get.char.R
@@ -1,7 +1,22 @@
+#' get a specific character from a word
+#'
+#' @param x a \code{character} \code{vector} of length >= 1
+#' @param idx the index into the characters, starting from 1
+#' 
+#' @return a character vector
+#' @author Mark Cowley, 2012-07-16
+#' @export
 get.char <- function(x, idx) {
-  unlist(strsplit(x, ""))[idx]
+	sapply(strsplit(x, ""), "[", idx)
 }
 
+#' convert a word into a character vector
+#'
+#' @param x a \code{character} \code{vector} of \code{length == 1}
+#' 
+#' @return a charcter vector of words, each of length 1.
+#' @author Mark Cowley, 2012-07-16
+#' @export
 to.char.array <- function(x) {
-  unlist(strsplit(x, ""))
+	unlist(strsplit(x, ""))
 }
