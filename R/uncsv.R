415ac0e212725c9df377bf4e0d9c34c7633dbd80
Mark Cowley <m.cowley0@gmail.com>
2012-07-16 04:49:26 +0000

    extracted uncsv from matlab.R extracted corceff and normrnd to mjcstats roxygenized

---
+++ R/uncsv.R
@@ -0,0 +1,23 @@
+#' uncsv
+#' 
+#' convert character vector where each element is composed of
+#' comma separated elements, into a character vector.
+#'
+#' @param x a character vector of length >= 1
+#' @param trim logical: trim whitespace? default=TRUE
+#' 
+#' @return a character vector of words, split on comma's
+#' 
+#' @author Mark Cowley, 2012-07-16
+#' @export
+#' @importFrom pwbc trim
+#' @examples
+#' uncsv("hello,quick,brown,fox")
+#' # [1] "hello" "quick" "brown" "fox"  
+uncsv <- function(x, trim=TRUE) {
+    res <- unlist(strsplit(x, ","))
+    if( trim )
+        res <- trim(res)
+
+    return( res )
+}
