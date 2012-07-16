a9664a0e8d3dd178c132c4c4110f66433a846ff2
Mark Cowley <m.cowley0@gmail.com>
2012-07-16 04:44:08 +0000

    refactored density.upper.triangle -> density_upper_tri refactored density.lower.triangle -> density_lower_tri renamed density.upper.tri.R -> density_tri.R roxygenized

---
+++ R/density_tri.R
@@ -0,0 +1,18 @@
+#' density triangles
+#' 
+#' Calculate the density of the values in the upper or lower triangle of x.
+#' 
+#' @inheritParams stats::density
+#' @param \dots arguments passed to density
+#' @author Mark Cowley, 4 May 2006
+#' @export
+#' @rdname density_tri
+density_upper_tri <- function(x, ...) {
+    density( x[upper.tri(x)], ...)
+}
+
+#' @export
+#' @rdname density_tri
+density_lower_tri <- function(x, ...) {
+    density( x[lower.tri(x)], ... )
+}
