e871570c1201ed85a6c9e557c0248b2bfad471ca
Mark Cowley <m.cowley0@gmail.com>
2012-07-17 06:07:23 +0000

    moved from metaGSEA

---
+++ R/is.int.R
@@ -0,0 +1,14 @@
+#' Checks which elements of a vector are integers.
+#' 
+#' This differs from is.integer which only checks the class of the entire
+#' vector.
+#' 
+#' @param x a vector. usually this will be a numeric vector, but can also be a
+#'   character
+#' @return a logical vector, same length as x, with TRUE if each element is an
+#'   integer
+#' @author Mark Cowley, 2009-11-30
+#' @export
+is.int <- function(x) {
+	suppressWarnings( as.integer(x) == x )
+}
