7cda29eb1191c5502e3d9174cd2a2e8a5aae6148
Mark Cowley <m.cowley0@gmail.com>
2012-07-19 03:54:00 +0000

    documentation updates

--- R/perl.oneliner.R
+++ R/perl.oneliner.R
@@ -1,15 +1,28 @@
-#' Function to wrap up a perl command such as 's|^.+\\t||' and execute it on the
+#' execute a perl one liner
+#' 
+#' execute a perl one liner, such as a regex search & replace (which is how
+#' i most often use this), eg \code{s|^.+\\t||} and execute it on the
 #' cmdline.
 #' 
-#' you can pass in multiple commands (such as multiple regex strings) applied
+#' you can pass in multiple commands (such as multiple regex search/replace strings) applied
 #' to one or multiple files.
 #' i've tested the usage of multiple regex commands applied to a single file.
+#' 
+#' @details commands
+#' The commands vector can either contain the leading perl command, eg
+#' \code{perl.oneliner("perl -pi -e 's|<DTG|\n  <DTG|g'", file)}\cr
+#' or just a pattern, eg\cr
+#' \code{perl.oneliner("'s|<DTG|\n  <DTG|g'", file)}\cr
+#' in which case we will add the leading \code{perl -pi -e }
 #' 
 #' @param cmds a character vector of commands
 #' @param files a character vector of paths to files. It is recyled to match the length
 #'   of cmds
 #' @param intern logical: capture the output of the command as an R character vector?
 #' @param debug logical: if \code{TRUE}, print the oneliner command before executing it
+#' 
+#' @return nothing
+#' 
 #' @author Mark Cowley
 #' @export
 perl.oneliner <- function(cmds, files, intern=FALSE, debug=FALSE) {
