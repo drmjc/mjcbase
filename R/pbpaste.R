4932918f2c71f680a751f98f73420a3df813c37a
Mark Cowley <m.cowley0@gmail.com>
2012-07-27 03:39:34 +0000

    added args to help with data.frame I/O bug fix in pbpaste on a data.frame

--- R/pbpaste.R
+++ R/pbpaste.R
@@ -21,8 +21,10 @@
 #' @param sep the separator to use if \code{class="matrix"}, or \code{class="data.frame"}
 #' @param ok logical: is it OK to be missing a final EOL?
 #' @param warn logical: warn if the clipboard is missing a final EOL
+#' @param header: logical: if \code{class="data.frame"} or \code{class="matrix"}, then is the first
+#'  line a header line?
 #' 
-#' @return a character vector of lines
+#' @return a data object from the clipboard, of type \code{class}.
 #' 
 #' @export
 #' @author Mark Cowley, 2011-07-22
@@ -53,11 +55,32 @@
 #' dput(letters, pipe("pbpaste", "w"))
 #' dget(pipe("pbcopy"))
 #' 
+#' # data.frame
+#' if( require(datasets) ) {
+#'   pbcopy(iris, col.names=TRUE)
+#'   head(pbpaste("data.frame", header=T))
+#'   #   Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+#'   # 2          5.1         3.5          1.4         0.2  setosa
+#'   # 3          4.9           3          1.4         0.2  setosa
+#'   # 4          4.7         3.2          1.3         0.2  setosa
+#'   # 5          4.6         3.1          1.5         0.2  setosa
+#'   # 6            5         3.6          1.4         0.2  setosa
+#'   # 7          5.4         3.9          1.7         0.4  setosa
+#'   head(pbpaste("data.frame", header=F))
+#'   #             V1          V2           V3          V4      V5
+#'   # 1 Sepal.Length Sepal.Width Petal.Length Petal.Width Species
+#'   # 2          5.1         3.5          1.4         0.2  setosa
+#'   # 3          4.9           3          1.4         0.2  setosa
+#'   # 4          4.7         3.2          1.3         0.2  setosa
+#'   # 5          4.6         3.1          1.5         0.2  setosa
+#'   # 6            5         3.6          1.4         0.2  setosa
+#' }
+#' 
 #' }
 #' @seealso \code{\link{readLines}}, \code{\link{pbcopy}}, the \dQuote{Clipboard} section within ?\code{\link{file}}
 #' @rdname pbpaste
 #' @aliases pbpaste
-pbpaste <- function(class="character", sep="\t", ok=TRUE, warn=TRUE) {
+pbpaste <- function(class="character", sep="\t", ok=TRUE, warn=TRUE, header=TRUE) {
 	isMac() || stop("This only works on Mac OSX")
 	
 	OUT <- pipe("pbpaste")
@@ -88,17 +111,21 @@ pbpaste <- function(class="character", sep="\t", ok=TRUE, warn=TRUE) {
 		close(OUT)
 	}
 	else {
-		as.df <- function(x, sep="\t") {
+		as.df <- function(x, sep="\t", header=TRUE) {
 			res <- strsplit(x, sep)
 			res <- t(as.data.frame(res))
 			res <- as.data.frame(res) # if data is all numeric, res becomes a matrix.
 			row.names(res) <- NULL
+			if( header ) {
+				res <- row2colnames(res, 1)
+			}
+			
 			res
 		}
 		res <- switch(class,
 			character=res,
-			data.frame=as.df(res, sep=sep),
-			matrix=as.matrix(as.df(res, sep=sep)),
+			data.frame=as.df(res, sep=sep, header=header),
+			matrix=as.matrix(as.df(res, sep=sep, header=header)),
 			as(res, class)
 		)
 	}
@@ -126,8 +153,18 @@ paste.from.clipboard <- function(class="character", sep="\t") pbpaste(class, sep
 #'   \code{data.frame} \tab \code{write.delim} \cr
 #'   other \tab \code{dput}
 #' }
+#' 
 #' @param x an object to be writen to the clipboard. see Details.
+#' @param row.names if \code{x} is a \code{matrix} or \code{data.frame}, then
+#' either \code{TRUE}, \code{FALSE}, \code{NULL} \code{NA}, or a \code{character(1)}. 
+#'  see \code{\link{write.delim}}. If \code{x} is a \code{list}, then write out the
+#'  list names in each row? see \code{\link{names}} arg in \code{\link{writeLines.list}}.
+#' Otherwise it's ignored.
+#' @param col.names if \code{x} is a \code{matrix} or \code{data.frame}, then write out
+#'  the col.names? \code{default=TRUE} Otherwise, it's ignored.
+#' 
 #' @return nothing
+#' 
 #' @export
 #' @author Mark Cowley, 2011-11-08
 #' @examples
@@ -141,16 +178,16 @@ paste.from.clipboard <- function(class="character", sep="\t") pbpaste(class, sep
 #' @seealso \code{\link{pbcopy}}, the \dQuote{Clipboard} section within ?\code{\link{file}}
 #' @rdname pbcopy
 #' @aliases pbcopy
-pbcopy <- function(x) {
+pbcopy <- function(x, row.names=FALSE, col.names=TRUE) {
 	isMac() || stop("This only works on Mac OSX")
 	
 	OUT <- pipe("pbcopy", "w")
-	if( is.list(x) )
-		writeLines.list(x, OUT)
+	if( is.matrix(x) || is.data.frame(x) )
+		write.delim(x, OUT, row.names=row.names, col.names=col.names)
 	else if( is.vector(x) && !is.list(x) )
 		writeLines(as.character(x), OUT)
-	else if( is.matrix(x) || is.data.frame(x) )
-		write.delim(x, OUT, row.names=!is.null(rownames(x)))
+	else if( is.list(x) )
+		writeLines.list(x, OUT, names=row.names)
 	else
 		dput(x, OUT)
 
