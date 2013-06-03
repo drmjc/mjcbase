1ab7b071d6b96385cf17301c6d9097f3d831416d
Mark Cowley <m.cowley0@gmail.com>
2013-06-03 05:26:15 +0000

    added @examples

--- R/collate.data.frame.R
+++ R/collate.data.frame.R
@@ -1,15 +1,5 @@
-# Collate the columns from two data.frames. ie produce a table that alternates columns from the 2 tables
-#
-# Parameters:
-#	x, y: two identically sized data.frames. Probably will work with matrices too.
-#
-# Value:
-#	a data.frame with ncol(x)*2
-#
-# Mark Cowley, 2009-12-16
-#
-
-
+#' Collate the columns from two data.frames
+#' 
 #' Collate the columns from two data.frames. ie produce a table that alternates
 #' columns from the 2 tables
 #' 
@@ -20,6 +10,10 @@
 #' @return a data.frame with ncol(x)*2
 #' @author Mark Cowley, 2009-12-16
 #' @export
+#' @examples
+#' a <- iris[1:5,]; colnames(a) <- letters[1:5]
+#' b <- iris[1:5,]; colnames(b) <- LETTERS[1:5]
+#' collate.data.frame(a, b)
 collate.data.frame <- function(x,y) {
 	stopifnot(ncol(x)==ncol(y))
 	stopifnot(nrow(x)==nrow(y))
