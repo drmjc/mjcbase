b3c12007433e4369e69660954e4338a6515dda04
Mark Cowley <m.cowley0@gmail.com>
2016-06-07 16:09:14 +1000

    updated way names are handled in rbind.list

--- R/rbind.list.R
+++ R/rbind.list.R
@@ -3,9 +3,20 @@
 #' You can either supply many variables that are row-bindable, (ie are
 #' the right dimension), or one list argument containing matrices that
 #' can be row-binded
+#'
+#' rownames will be set to <list element name>.<original row name>
+#'
+#' Often when rbind-ing tables of data together, you want to tag all rows 
+#' from each table with the sample to which they belong. You can specify
+#' \code{name2colname}, which will add a new column and track the origin
+#' of those rows.
+#'
 #' @param \dots either a single list of tables, or multiple tables to rbind, where table means
 #' \code{matrix} or \code{data.frame}
 #' @param sep currently unused
+#' @param name2colname Add a new column to each table before rbinding it? If not \code{NULL}
+#'  then a new column will be added, the values correspond to `names(x)`, and the colname
+#'  is `name2colname`.
 #' @return a large table with all elements in the list rbind-ed 
 #' @author Mark Cowley, 5 April 2006
 #' @export
@@ -18,12 +29,12 @@ rbind.list <- function(..., sep=".") {
         names(x) <- LETTERS[1:length(x)]
 
     res <- NULL
-    #     names <- NULL
     for(i in 1:length(x)) {
-        res <- rbind(res, x[[i]])
-    #         names <- c(names, paste(rownames(x[[i]]), names(x)[i], sep=sep))
+      rownames(x[[i]]) <- paste(names(x)[i], rownames(x[[i]]), sep=".")
+      res <- rbind(res, x[[i]])
     }
 
-    rownames(res) <- names(x)
     return(res)
 }
+# CHANGELOG
+# 2016-04-14: update rownames
